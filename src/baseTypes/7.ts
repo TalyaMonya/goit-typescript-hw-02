2b467cb45d2d533dd4aeecee81191df54c4ae68f
TalyaMonya <128909188+TalyaMonya@users.noreply.github.com>
2023-11-23 15:46:42 +0200

    Done ts-hw-02

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,23 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum WeekDays {
+  monday,
+  tuesday,
+  wednesday,
+  thursday,
+  friday,
+  saturday,
+  sunday,
+};
+
+function isWeekend(day: WeekDays): boolean {
+  return day === WeekDays.saturday || day === WeekDays.sunday;
+}
+
+if (isWeekend(WeekDays.monday)) {
+  console.log("Сьогодні вихідний день");
+} else {
+  console.log("Сьогодні робочий день");
+}
--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,14 +1,17 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
+interface Obj {
+  title: string
+}
 
-class Component {
+class Component<T> {
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
+class Page extends Component<Obj> {
   pageInfo () {
     console.log(this.props.title);
   }
